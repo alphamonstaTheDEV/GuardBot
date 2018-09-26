593cf8f1321e0467ce34ef3681e968ecd85fd873
Can <canerdal2003@hotmail.com>
2018-09-26 17:38:58 +0300

    Fixed message repy

--- utils/auto.js
+++ utils/auto.js
@@ -6,6 +6,9 @@ const config = require("../config.json");
 
 module.exports.actionTaken = (message, action, member, reason) => {
 	if (!reason) return message.channel.send(`**${member.user.tag}\` (${member.user.id})\`** has been ${action} by **${message.author.tag}**`);
+	if (action === "force banned") {
+		return logChannel.send(`🛡 **${member}\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`** | Reason: \`${reason}\``);
+	} 
 	return message.channel.send(`**${member.user.tag}\` (${member.user.id})\`** has been ${action} by **${message.author.tag}** | Reason: \`${reason}\``);
 
 }
@@ -14,11 +17,13 @@ module.exports.log = (message, action, member, reason) => {
 	let logChannel = message.guild.channels.find("name", "guardbot-log");
 	if (!logChannel) return;
 	if (action === "lock") {
-		logChannel.send(`:lock: <#${message.channel.id}> has been locked by **${message.author.tag}\` (${message.author.id})\`** for ${member}`);
+		return logChannel.send(`:lock: <#${message.channel.id}> has been locked by **${message.author.tag}\` (${message.author.id})\`** for ${member}`);
 	} else if (action === "unlock") {
-		logChannel.send(`:unlock: <#${message.channel.id}> has been unlocked by **${message.author.tag}\` (${message.author.id})\`**`);
-	} else if (action === "clear") {
+		return logChannel.send(`:unlock: <#${message.channel.id}> has been unlocked by **${message.author.tag}\` (${message.author.id})\`**`);
+	} /*else if (action === "clear") {
 		logChannel.send(`${message.author.tag}\` (${message.author.id})\``)
+	}*/ else if (action === "force banned") {
+		return logChannel.send(`🛡 **${member}\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`** | Reason: \`${reason}\``);
 	} else {
 		if (!reason) return logChannel.send(`🛡 **${member.user.tag}\` (${member.user.id})\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`**`);
 		return logChannel.send(`🛡 **${member.user.tag}\` (${member.user.id})\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`** | Reason: \`${reason}\``);
