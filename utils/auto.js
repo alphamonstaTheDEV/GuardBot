fcb076304f5114d12d33bf0f3c3096748e92770c
Can <canerdal2003@hotmail.com>
2018-09-29 16:00:32 +0300

    Fixed logging with the clear

--- utils/auto.js
+++ utils/auto.js
@@ -16,6 +16,9 @@ module.exports.actionTaken = (message, action, member, reason) => {
 module.exports.log = (message, action, member, reason) => {
 	let logChannel = message.guild.channels.find("name", "guardbot-log");
 	if (!logChannel) return;
+	if (!reason) {
+		reason = "Not given."
+	}
 	if (action === "lock") {
 		return logChannel.send(`:lock: <#${message.channel.id}> has been locked by **${message.author.tag}\` (${message.author.id})\`** for ${member}`);
 	} else if (action === "unlock") {
@@ -24,6 +27,8 @@ module.exports.log = (message, action, member, reason) => {
 		logChannel.send(`${message.author.tag}\` (${message.author.id})\``)
 	}*/ else if (action === "force banned") {
 		return logChannel.send(`🛡 **${member}\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`** | Reason: \`${reason}\``);
+	} else if (action === "clear") {
+		return logChannel.send(`🛡 **${message.author.tag}\` (${message.author.id})\`** has bulk deleted <#${message.channel.id}> (${message.channel.id})`);
 	} else {
 		if (!reason) return logChannel.send(`🛡 **${member.user.tag}\` (${member.user.id})\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`**`);
 		return logChannel.send(`🛡 **${member.user.tag}\` (${member.user.id})\`** has been ${action} by **${message.author.tag}\` (${message.author.id})\`** | Reason: \`${reason}\``);
