4f4b3d5e19d8a85151d188a1a050067cf52bf67c
Can <canerdal2003@hotmail.com>
2018-10-09 23:47:07 +0300

    OwO

--- cmds/help.js
+++ cmds/help.js
@@ -6,7 +6,6 @@ const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
     let cmds = [];
-    message.delete()
     await fs.readdir("./cmds/", async (err, files) => {
         let jsfiles = files.filter(f => f.split(".").pop() === "js");
         jsfiles.forEach((f) => {
@@ -17,7 +16,7 @@ module.exports.run = async (client, message, args) => {
     });
     if(!args[0]) return message.channel.send("/help <command>")
     const cmd = require(`./${args[0].toLowerCase()}.js`)
-    if(!cmd.help) return message.channel.send("Unknown command. Commands: `" + cmds.join(", ") + "`");
+    if(!cmd.help || !cmd) return message.channel.send("Unknown command. Commands: `" + cmds.join(", ") + "`");
     let help = new Discord.RichEmbed()
     .setTitle(`${args[0]} command`)
     .setDescription(cmd.help.description)
