0cf7ebb4b8adc459fc6656a65ba60b51167206c7
Can <canerdal2003@hotmail.com>
2018-10-10 18:49:02 +0300

    sss

---
+++ utils/errors.js
@@ -0,0 +1,5 @@
+const fs = require(`fs`);
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+const auto = require("../utils/auto.js");
+const config = require("../config.json");
