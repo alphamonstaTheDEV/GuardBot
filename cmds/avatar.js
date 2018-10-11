b2d9171b4a84b67af25b2822c5b8efbee833babb
Can <canerdal2003@hotmail.com>
2018-10-11 19:02:42 +0300

    Fixesss

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -6,7 +6,7 @@ const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
     let user = message.mentions.users.first();
-    if (!message.mentions.users.first() && args[0]) {user = client.users.get(args[0])} else {user = message.author}
+    if (!message.mentions.users.first() && args[0]) { user = client.users.get(args[0]) || message.author}
     let embed = new Discord.RichEmbed()
     .setColor(0x0000ff)
     .setTitle("Avatar of " + user.tag)
