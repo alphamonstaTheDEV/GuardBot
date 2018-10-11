5f74203315cdf7576dd555b8342b4e496289ca16
Can <canerdal2003@hotmail.com>
2018-10-11 18:59:04 +0300

    Repair

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -5,19 +5,12 @@ const auto = require("../utils/auto.js");
 const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
-    if (!args[0]) {let user = message.member} 
-    else {
-        user = message.mentions.members.first()
-    if (!user) {
-        user = message.guild.members.get(args[0])
-        if (!user) return help.usage;
-    }
-}
-
+    let user = message.mentions.users.first();
+    if (!message.mentions.users.first() && args[0]) {user = client.users.get(args[0])} else {user = message.author}
     let embed = new Discord.RichEmbed()
     .setColor(0x0000ff)
-    .setTitle("Avatar of " + user.user.tag)
-    .setImage(user.user.displayAvatarURL)
+    .setTitle("Avatar of " + user.tag)
+    .setImage(user.displayAvatarURL)
     message.channel.send(embed)
 }
 module.exports.help = {
