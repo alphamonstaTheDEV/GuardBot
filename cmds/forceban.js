5e52480a48c1ce0b35a2d1d35db956316407cdb2
Can <canerdal2003@hotmail.com>
2018-11-03 23:30:38 +0300

    a

--- cmds/forceban.js
+++ cmds/forceban.js
@@ -5,7 +5,7 @@ const auto = require("../utils/auto.js");
 const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
+    if (!message.member.hasPermission("BAN_MEMBERS")) return;
     let user = args[0];
     let reason = args.slice(1).join(" ");
     client.fetchUser(user).then(id => {
