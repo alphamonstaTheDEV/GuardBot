973884262c5f004c444440a4ece8ff953bfbf48f
Can <canerdal2003@hotmail.com>
2018-11-07 22:28:41 +0300

    a

--- cmds/botreport.js
+++ cmds/botreport.js
@@ -8,21 +8,18 @@ module.exports.run = async (client, message, args) => {
     let bugChannel = client.channels.get("498009184097533963");
     if(message.channel.id = bugChannel.id) {
         let command = args[0]
+        let bugMessage = bugChannel.fetchMessage(args[1]);
+        if (!bugMessage) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
+        if (bugMessage.author.id == message.author.id) return;
         switch(command) {
             case "approve":
-                let bugMessage = bugChannel.fetchMessage(args[1]);
-                if(!bugMessage) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
                 bugMessage.edit(bugMessage.content + `\n:white_check_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
             break;
             case "deny":
-                let bugMessage1 = bugChannel.fetchMessage(args[1]);
-                if (!bugMessage1) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
-                bugMessage1.edit(bugMessage1.content + `\n:negative_squared_cross_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
+                bugMessage.edit(bugMessage.content + `\n:negative_squared_cross_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
             break;
             case "note":
-                let bugMessage2 = bugChannel.fetchMessage(args[1]);
-                if (!bugMessage2) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
-                bugMessage2.edit(bugMessage2.content + `\n:pencil:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
+                bugMessage.edit(bugMessage.content + `\n:pencil:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
             break;
         }
     }
