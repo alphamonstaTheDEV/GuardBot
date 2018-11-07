fbaca3e1958e70e53096af0210595749d935dfb9
Can <canerdal2003@hotmail.com>
2018-11-07 20:18:41 +0300

    save

--- utils/auto.js
+++ utils/auto.js
@@ -58,7 +58,7 @@ module.exports.fetchInf = (message, memberID, infType) => {
 	warning_channel.fetchMessages().then(messages => {
 		let fmessages = messages.filter(msg => msg.content.startsWith(memberID))
 		if(infType == "all") {
-			arrayOfInfs = msgs.map(msg => { return msg.content })
+			arrayOfInfs = msg.map(msgss => { return msgss.content })
 			} else {
 			let msgs = fmessages.filter(msg => msg.content.startsWith(`${memberID} ${infType}`))
 			arrayOfInfs = msgs.map(msg => {return msg.content})
