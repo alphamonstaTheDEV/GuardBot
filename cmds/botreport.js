ed0724e3fb5011a46a66281e8e16e34d2266cfbe
Can <canerdal2003@hotmail.com>
2018-11-08 21:24:03 +0300

    test

--- cmds/botreport.js
+++ cmds/botreport.js
@@ -8,24 +8,26 @@ module.exports.run = async (client, message, args) => {
     let bugChannel = client.channels.get("498009184097533963");
     if(message.channel.id == bugChannel.id) {
         let command = args[0]
-        let bugMessage = bugChannel.fetchMessage(`${args[1]}`);
-        if (!bugMessage) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
-        //if (bugMessage.author.id == message.author.id) return;
-        switch(command) {
-            case "approve":
-                bugMessage.edit(bugMessage.content + `\n:white_check_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
-            break;
-            case "deny":
-                bugMessage.edit(bugMessage.content + `\n:negative_squared_cross_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
-            break;
-            case "note":
-                bugMessage.edit(bugMessage.content + `\n:pencil:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
-            break;
-            default:
-                message.channel.send("Error: Unknown command.").then(msg => msg.delete(10000));
-        }
+        bugChannel.fetchMessage(`${args[1]}`).then(bugMessage => {
+            if (!bugMessage) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
+            //if (bugMessage.author.id == message.author.id) return;
+            switch (command) {
+                case "approve":
+                    bugMessage.edit(bugMessage.content + `\n:white_check_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
+                    break;
+                case "deny":
+                    bugMessage.edit(bugMessage.content + `\n:negative_squared_cross_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
+                    break;
+                case "note":
+                    bugMessage.edit(bugMessage.content + `\n:pencil:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
+                    break;
+                default:
+                    message.channel.send("Error: Unknown command.").then(msg => msg.delete(10000));
+            }
+        })
+    } else {
+        bugChannel.send(`**───────────────────\n${message.author.tag}** reported:\n ${args.join(" ")}\n\n The bug report above needs to be approved.`);
     }
-    bugChannel.send(`**───────────────────\n${message.author.tag}** reported:\n ${args.join(" ")}\n\n The bug report above needs to be approved.`);
 
 }
 
