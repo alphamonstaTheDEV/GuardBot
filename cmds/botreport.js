70b62dea59e2ce3828c5b868121a0f4a222436a8
Can <canerdal2003@hotmail.com>
2018-11-08 21:14:56 +0300

    f

--- cmds/botreport.js
+++ cmds/botreport.js
@@ -8,9 +8,9 @@ module.exports.run = async (client, message, args) => {
     let bugChannel = client.channels.get("498009184097533963");
     if(message.channel.id == bugChannel.id) {
         let command = args[0]
-        let bugMessage = bugChannel.fetchMessage(args[1]);
+        let bugMessage = bugChannel.fetchMessage(`${args[1]}`);
         if (!bugMessage) return message.channel.send("Error: Couldn't fetch that report.").then(msg => msg.delete(10000));
-        if (bugMessage.author.id == message.author.id) return;
+        //if (bugMessage.author.id == message.author.id) return;
         switch(command) {
             case "approve":
                 bugMessage.edit(bugMessage.content + `\n:white_check_mark:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
@@ -21,6 +21,8 @@ module.exports.run = async (client, message, args) => {
             case "note":
                 bugMessage.edit(bugMessage.content + `\n:pencil:**${message.author.tag}** | \`${args.slice(3).join(" ")}\``)
             break;
+            default:
+                message.channel.send("Error: Unknown command.").then(msg => msg.delete(10000));
         }
     }
     bugChannel.send(`**───────────────────\n${message.author.tag}** reported:\n ${args.join(" ")}\n\n The bug report above needs to be approved.`);
@@ -31,5 +33,5 @@ module.exports.help = {
     "description": "Report bugs to GuardBot Developers.",
     "usage": "/botreport --desc <short description of the bug>",
     "perms": "USER",
-    "state": ""
+    "state": "functional"
 }
