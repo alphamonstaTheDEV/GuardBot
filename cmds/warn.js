28585a67f81490dec78fd2bc4a9a16434ee7bc3e
Can <canerdal2003@hotmail.com>
2018-11-10 15:37:03 +0300

    minor fixes

--- cmds/warn.js
+++ cmds/warn.js
@@ -12,7 +12,7 @@ module.exports.run = async (client, message, args) => {
     
     if (user == message.member) return;
 
-    if (!warning_channel) return user.user.send("You have been warned in `" + message.guild.name +"`\n Reason: `" + reason + "`");
+    if (!warning_channel) return;
     let json = JSON.stringify({ user: user.id, action: "warning", reason: reason, moderator: message.author.id, date: Date.now()})
     await warning_channel.send(user.id + json)
     auto.log(message, "warned", user, reason)
@@ -24,34 +24,3 @@ module.exports.help = {
     "perms": "MANAGE_MESSAGES",
     "state": "functional"
 }
-
-
-/*let reportChannel = message.guild.channels.find("name", "mod-log") || message.guild.channels.find("name", "log")
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
-    let user = message.mentions.users.first();
-    let reason = args.slice(1).join(" ");
-    if (!user) {
-        if (!parseInt(args[0])) return message.channel.send("**ERROR**: \`/warn **<user>** <reason>\`");
-        user = message.guild.users.get(args[0]);
-    }
-    if (user == message.author) return;
-
-    try {
-        let embed = new Discord.RichEmbed()
-            .setColor(0x00ff00)
-            .setTitle("Warn")
-            .addField("Warn Received From:", `<@${message.author.id}>`)
-            .addField("Target User:", `<@${user.id}>`)
-            .addField("Reason:", reason)
-            .setTimestamp()
-            .setThumbnail("https://ipvm-uploads.s3.amazonaws.com/uploads/5ac5/9b52/Critical.png")
-            .setFooter(client.user.username, client.user.avatarURL)
-        reportChannel.send(embed)
-    } catch (err) {
-        try {
-            user.send("You have been warned. Reason: `" + reason + "`");
-        } catch (err) { }
-    }
-    auto.log(message, "warned", message.mentions.members.first(), reason)
-    auto.actionTaken(message, "warned", message.mentions.members.first(), reason)
-     */
