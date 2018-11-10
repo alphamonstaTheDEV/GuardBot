6c7f02d3dc5921484bb765cfe0d60617b7072c1d
Can <canerdal2003@hotmail.com>
2018-11-10 13:34:56 +0300

    a

--- cmds/infs.js
+++ cmds/infs.js
@@ -3,12 +3,26 @@ const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 const auto = require("../utils/auto.js");
 const config = require("../config.json");
-
+const table = require("table");
 module.exports.run = async (client, message, args) => {
     let data, output;
+    let user = auto.getUser(message, args);
+    if (user == "error") return message.channel.send(help.usage)
     data = [
-        ["User", "Action", "Reason", "Moderator"]
+        ["Created At", "User", "Action", "Moderator", "Reason"]
     ]
+
+    await auto.fetchInf(message, user.id, "all").then(array => {
+        array.map(inf => {
+            let date = new Date(inf.date)
+            let mod = client.users.get(inf.moderator).tag || inf.moderator;
+            data.push([date, user.user.tag, inf.action, inf.moderator, inf.reason])
+        })
+    })
+    output = table.table(data);
+    return message.channel.send("```" + output + "```");
+
+
 }
 
 module.exports.help = {
